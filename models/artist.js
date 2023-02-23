b04e2b3efbc53a4dae330f22a3afb31a4bcd6176
paula <paula_ribe93@hotmail.com>
2023-02-23 13:39:03 +0100

    Ejemplo de sequelize con una tienda de tatuajes

---
+++ models/artist.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Artist extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Artist.belongsToMany(models.User,
+        {through: {model: models.Appointment, unique: false}}
+      )
+    }
+  }
+  Artist.init({
+    name: DataTypes.STRING,
+    style: DataTypes.STRING,
+    instagram: DataTypes.STRING,
+    pricing: DataTypes.INTEGER,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Artist',
+  });
+  return Artist;
+};
